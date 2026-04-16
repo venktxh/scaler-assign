c2b43dbb196076a0dd3acdd68ef2bca64c8fc992
venkatesh-13 <venkeysanikommu@gmail.com>
2026-04-16 23:09:24 +0530

    added ssl and db config for deployment

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -6,8 +6,15 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+
   waitForConnections: true,
   connectionLimit: 10,
+
+  // 🔥 REQUIRED for Aiven
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
